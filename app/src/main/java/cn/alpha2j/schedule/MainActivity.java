1aaf25115e5a4b456bd0f9c69d2e824a24dc3cc8
Alpha2J <3287566354@qq.com>
2017-10-25 11:52:54 +0800

    完成了主页toolbar图标设置

--- app/src/main/java/cn/alpha2j/schedule/MainActivity.java
+++ app/src/main/java/cn/alpha2j/schedule/MainActivity.java
@@ -2,6 +2,10 @@ package cn.alpha2j.schedule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add:
+                Toast.makeText(this, "点击了添加按钮", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.setting:
+                Toast.makeText(this, "点击了设置图标", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+
+        return true;
     }
 }
